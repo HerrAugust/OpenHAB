9e58a4c3e2f5056f3e0c9b35a517af23e0b733cd
HerrAugust <agostino.mascitti@outlook.it>
2017-02-23 14:19:14 +0100

    storedecision bugfix

--- StoreDecision/src/storedecision/StoreDecision.java
+++ StoreDecision/src/storedecision/StoreDecision.java
@@ -5,6 +5,7 @@
  */
 package storedecision;
 
+import Logger.Logger;
 import Storage.Storage;
 
 /**
@@ -20,10 +21,13 @@ public class StoreDecision {
         if(args.length == 0)return;
         Storage s = new Storage();
         
+        
         String val = "\"" + args[0];
         if(args.length > 1)
         	val += " " + args[1];
         val += "\"";
+        Logger l = new Logger(System.getProperty("user.home") + "\\Documents\\Ruscio\\OpenHAB", "storedecision.log");
+        l.writeln(val);
         s.write("decision", "value", val);        
     }
     
